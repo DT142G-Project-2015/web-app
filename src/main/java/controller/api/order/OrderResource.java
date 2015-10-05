4a92d968339a394695076b8f77285220a37aeb92
Joel Edström <joel.edstrom@gmail.com>
2015-10-05 18:47:10 +0200

    MySQL Fix

--- src/main/java/controller/api/order/OrderResource.java
+++ src/main/java/controller/api/order/OrderResource.java
@@ -128,8 +128,8 @@ public class OrderResource {
             "I2.description AS sub_description, I2.price AS sub_price, I2.type AS sub_type, " +
             "I2.id AS sub_id " +
             "FROM item I2, receipt_item RI2, receipt_item_item RII " +
-            "WHERE I2.id = RI2.item_id AND RI2.id = RII.receipt_item_id_sub) " +
-            "ON dom_receipt_item_id = RI.id " +
+            "WHERE I2.id = RI2.item_id AND RI2.id = RII.receipt_item_id_sub) AS D" +
+            "ON D.dom_receipt_item_id = RI.id " +
 
             "WHERE I.id = RI.item_id " +
             "AND RG.id = RGI.receipt_group_id " +
