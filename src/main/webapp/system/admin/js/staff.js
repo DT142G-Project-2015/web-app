fb42ee60ed5e455681a0de2db9b531ff48565fb4
gustavastrom <gustav.j.astrom@gmail.com>
2015-10-06 10:04:03 +0200

    updated staff.js

--- src/main/webapp/system/admin/js/staff.js
+++ src/main/webapp/system/admin/js/staff.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
         }).done(function(data){
 
             data.forEach(function(staff){
-                alert(staff[0]);
+                alert(JSON.stringify(staff));
             });
 
         });
