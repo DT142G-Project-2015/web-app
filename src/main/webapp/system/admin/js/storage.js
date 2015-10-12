35f9ca3a431456af923d7fa93c502e6348164092
sepe1300 <sepe1300@student.miun.se>
2015-10-12 14:39:45 +0200

    Updating inventory and storage

--- src/main/webapp/system/admin/js/storage.js
+++ src/main/webapp/system/admin/js/storage.js
@@ -18,7 +18,44 @@ $(document).ready(function(){
             dataType: 'json'
         }).done(function(data){
             var rendered = Mustache.render(template, {articles: data})
+           $('#content .section').empty();
            $('#content .section').append(rendered);
+
+            //Is used to delete an article and fetching the new menu.
+            $('.delete-btn').click(function() {
+                var storage_id = $(this).closest('.inventory-list-item').data('id');
+                //alert(JSON.stringify(storage_id));
+                $.ajax({
+                    url: '../../api/storage/' + storage_id,
+                    type: 'DELETE',
+                    dataType: 'text'
+                }).done(function() {
+                    getStorage(); //Used to fetch the new menu.
+                });
+            });
+
+            //Is used to update an article and fetching the new menu.
+            $('.change-btn').click(function() {
+                var storage_id = $(this).closest('.inventory-list-item').data('id');
+
+                alert(JSON.stringify(storage_id));
+
+                var storage = {}
+                $('.inventory-list .change-btn form :input').each(function(index, element){
+                    storage[element.id] = element.value;
+                });
+
+                $.ajax({
+                    url: '../../api/storage/' + storage_id,
+                    type: 'PUT',
+                    dataType: JSON.stringify(storage)
+                }).done(function() {
+                    alert('Uptaded menu');
+                    getStorage(); //Used to fetch the new menu.
+                });
+            });
+
+
         });
     }
 
@@ -38,45 +75,11 @@ $(document).ready(function(){
             data: JSON.stringify(storage)
         }).done(function(addedStorage){
         });
-
     }
-/*
-    function deleteStorage(){
-
-        var storage = {}
-        $('#add-storage form :input').each(function(index, element){
-            storage[element.id] = element.value;
-        });
-
-        alert(JSON.stringify(storage));
-        $('#item_list .delete_btn').click(function() {
-                var item_id = $(this).closest('.menu_item').data('id');
-                $.ajax({
-                    url: '../../api/storage',
-                    type: 'DELETE',
-                    dataType: 'json',
-                    data: JSON.stringify(storage)
-                }).done(function(addedStorage){
-                    alert('Borttagen..');
-                });
-
-
-    }*/
-
-    $('.inventory-list .delete_btn').click(function() {
-       var storage_id = $(this).closest('.inventory-list-item').data('id');
-
-        $.ajax({
-            url: '../../api/storage/' + storage_id,
-            type: 'DELETE',
-            dataType: 'text'
-        }).done(function() {
-            alert('removed');
-        });
-    });
 
     $("#add-storage-btn").click(function(){
         addStorage();
+        getStorage();
     });
 
 
