82c95d7f852e3552385ce07a5fbebb1315588d70
Joel Edström <joel.edstrom@gmail.com>
2015-10-20 00:13:50 +0200

    Add ShiftResource

--- src/main/java/controller/api/schedule/ScheduleResource.java
+++ src/main/java/controller/api/schedule/ScheduleResource.java
@@ -1,11 +1,10 @@
 package controller.api.schedule;
 
-import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("schedule")
 @Produces(MediaType.APPLICATION_JSON)
 public class ScheduleResource {
+
 
 }
--- src/main/java/model/Shedule.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-/**
- * Created by qwertyuiop on 2015-10-19.
- */
-public class Shedule {
-}
