f642f85d1e098c66d9cef39f7618977b1130d6b7
gustavastrom <gustav.j.astrom@gmail.com>
2015-10-20 18:59:41 +0200

    fixade lite

--- src/main/webapp/system/admin/js/schedule.js
+++ src/main/webapp/system/admin/js/schedule.js
@@ -21,26 +21,41 @@ $(document).ready(function(){
             dataType: 'json'
         }).done(function(data){
 
-            $('#standard-shift-holder table').empty();
-
+            $('#standard-shift-holder table').html('<tr><td>Från</td><td>Till</td><td>Bokade</td><td>Beskrivning</td><td>Upprepar</td><td>Åtgärder</td></tr>');
+            $('.day').empty();
 
             var now = new Date();
 
+            var zIndex = 0;
             $.each(data, function(i, shift){
 
                 var renderedShifts = Mustache.render(shiftTemplate, {shifts: shift});
                 $('#standard-shift-holder table').append(renderedShifts);
 
-                var shiftDate = new Date(shift.start);
+                var shiftStart = new Date(shift.start);
+                var shiftStop = new Date(shift.stop);
 
-                if(shiftDate.format('Y-W') == now.format('Y-W')){
-                    alert('shift in current week: ' + now.format('Y-W'));
+                shift.zIndex = zIndex;
+
+                if(shift.repeated == true){
+
+                    shift.start = shiftStart.format('H:i');
+                    shift.stop = shiftStop.format('H:i');
+                    shift.marginTop = ((parseFloat(shiftStart.format('H')) + parseFloat(shiftStart.format('i'))/60) - 8)*25;
+                    shift.height = (((parseFloat(shiftStop.format('H')) + parseFloat(shiftStop.format('i'))/60)-8)*25)-shift.marginTop;
+                    var renderedShift = Mustache.render(scheduleTemplate, {shift: shift});
+                    $('#' + shiftStart.format('D')).append(renderedShift);
                 }
+                else if(shiftStart.format('Y-W') == now.format('Y-W')){
 
-                var renderedShift = Mustache.render(scheduleTemplate, {shift: data});
-                $('#mon').html(renderedShift);
-
-                alert(JSON.stringify(shift));
+                    shift.start = shiftStart.format('H:i');
+                    shift.stop = shiftStop.format('H:i');
+                    shift.marginTop = ((parseFloat(shiftStart.format('H')) + parseFloat(shiftStart.format('i'))/60) - 8)*25;
+                    shift.height = (((parseFloat(shiftStop.format('H')) + parseFloat(shiftStop.format('i'))/60)-8)*25)-shift.marginTop;
+                    var renderedShift = Mustache.render(scheduleTemplate, {shift: shift});
+                    $('#' + shiftStart.format('D')).append(renderedShift);
+                }
+                zIndex++;
             });
 
             events();
@@ -52,12 +67,12 @@ $(document).ready(function(){
         $('#loader').show();
 
         var shift = {}
-        $('#add-shift-holder form input').each(function(index, element){
+
+        $('#add-shift-holder form :input').each(function(index, element){
             shift[element.id] = element.value;
         });
 
         alert(JSON.stringify(shift));
-
         $.ajax({
             url: '../../api/shift',
             type: 'POST',
@@ -65,7 +80,6 @@ $(document).ready(function(){
             data: JSON.stringify(shift)
         }).done(function(addedShift){
             getShifts();
-            alert('success');
         }).fail(function(){
             $('#loader').fadeOut(200);
             alert('fail');
